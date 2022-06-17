f92539f7441c9175b4753c89985d8e70d26d168a
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-06-17 11:40:55 -0400

    delete service completed and tested

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,13 +18,17 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     // this.tasks = this.taskService.getTasks()
 
-    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+    this.taskService
+      .getTasks()
+      .subscribe((tasks) => (this.tasks = tasks));
     // getTasks now is a Observable, so we have to subcribe to it. this is like a promise .then() the return value (now tasks ) will be set equal to the tasks we got back from the observale
   }
   // this will be fired off right away as I left it in ngOnInit
 
   deleteTask(task: Task) {
-    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id! === task.id)))
+    this.taskService
+      .deleteTask(task)
+      .subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)))
   }
-  // similar to getTasks, I changed this to deleteTask with specific task as an argument. This wont give me anything back but I want it to disappear from the UI
+  // similar to getTasks, I changed this to deleteTask with specific task as an argument. This wont give me anything back but I want such task to disappear from the UI by filter it out. Therefore the return tasks are not equal to the id of the deleted task.
 }
