25e69e0851bd3cabccf40215765f5dbc6765496f
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-06-20 12:01:13 -0400

    successfully add a task through server through form, sending alert to user if the text box is empty

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  text: string;
+  day: string;
+  reminder: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if(!this.text){
+      alert('Please add a task!');
+      return;
+    }
+  }
+
 }
+// in order to use form, all the properties have to be declared in the component, we also brough in the Forms Module in the app.module.ts
